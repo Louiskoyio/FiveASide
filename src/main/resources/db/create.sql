b76162b176c2b020ad523941c7a96cc32ffd77fc
Louiskoyio <koyiolouis@gmail.com>
2019-09-08 13:49:31 +0300

    edited dao files and tests

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -3,11 +3,11 @@ SET MODE PostgreSQL;
 CREATE TABLE IF NOT EXISTS heroes (
  id int PRIMARY KEY auto_increment,
  name VARCHAR,
- age NUMBER,
+ age INT,
  special_power VARCHAR,
  weakness VARCHAR,
- squad_id NUMBER,
- overall_rating NUMBER
+ squad_id INT,
+ overall_rating INT
  );
 
 CREATE TABLE IF NOT EXISTS squads (
@@ -16,8 +16,12 @@ CREATE TABLE IF NOT EXISTS squads (
   objective VARCHAR
 );
 
+DELETE FROM heroes WHERE id > 0;
+INSERT INTO heroes (name,age,special_power,weakness,squad_id,overall_rating) VALUES ('The Flash',46, 'Speed','bananas',1,80);
+INSERT INTO heroes (name,age,special_power,weakness,squad_id,overall_rating) VALUES ('Oliver "Green Arrow" Queen',30, 'Arrows','Mirakuru Soldiers',1,87);
+/*
 
 INSERT INTO heroes (name,age,special_power,weakness,squad_id,overall_rating) VALUES ('The Flash',46, 'Speed','bananas',1,80);
 INSERT INTO heroes (name,age,special_power,weakness,squad_id,overall_rating) VALUES ('Oliver "Green Arrow" Queen',30, 'Arrows','Mirakuru Soldiers',1,87);
 INSERT INTO squads (name, objective) VALUES ('Messi-Griezmann-Suarez', 'Deliver the UEFA Champions League');
-INSERT INTO squads (name, objective) VALUES ('Becker-Matip-Virgil', 'Defend the UEFA Champions League');
+INSERT INTO squads (name, objective) VALUES ('Becker-Matip-Virgil', 'Defend the UEFA Champions League');*/
